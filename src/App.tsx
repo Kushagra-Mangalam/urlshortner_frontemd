898f8953622d9a38cf19b5d3c03bf023bccf2e98
flashyman261 <flashyman261@gmail.com>
2026-07-16 04:02:30 +0530

    change in backend url

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
     setCopied(false);
 
     try {
-      const backendUrl = import.meta.env.VITE_BACKEND_URL;
+      const backendUrl = import.meta.env.VITE_API_URL;
       const response = await axios.post<URLResponse>(`${backendUrl}/shorten`, {
         original_url: originalUrl
       });
